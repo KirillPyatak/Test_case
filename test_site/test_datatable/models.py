6060684f83b369bac7b55272360264c5df1bff29
KirillPyatak <92531775+KirillPyatak@users.noreply.github.com>
2023-09-19 19:32:41 +0300

    Test case

--- test_site/test_datatable/models.py
+++ test_site/test_datatable/models.py
@@ -1,9 +1,12 @@
+# models.py
 from django.db import models
 
-# Create your models here.
 class Modalities(models.Model):
     short_code = models.CharField(max_length=10, verbose_name='Короткий код')
     name = models.CharField(max_length=250, verbose_name='Наименование модальности')
+
+    def __str__(self):
+        return self.name
 
 class Studies(models.Model):
     patient_fio = models.CharField(max_length=250, verbose_name='ФИО пациента')
@@ -11,3 +14,6 @@ class Studies(models.Model):
     study_uid = models.UUIDField(verbose_name='Идентификатор исследования во внешней системе')
     study_date = models.DateTimeField(verbose_name='Дата и время исследования')
     study_modality = models.ForeignKey(Modalities, on_delete=models.DO_NOTHING, verbose_name='Модальность исследования')
+
+    def __str__(self):
+        return self.patient_fio
